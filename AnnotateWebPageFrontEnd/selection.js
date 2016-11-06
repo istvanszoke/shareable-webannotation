33d9febefc13b8af1ccb1ed7ca8cd2d72cc5d484
Istvan Szoke <istvan.szoken@gmail.com>
2016-11-06 16:04:52 +0100

    Extension now prints out the location of the selection in the DOM

--- AnnotateWebPageFrontEnd/selection.js
+++ AnnotateWebPageFrontEnd/selection.js
@@ -4,17 +4,59 @@ chrome.extension.onMessage.addListener(function (request, sender, sendResponse) 
         sendResponse({ data: window.getSelection().toString() });
     if (request.method == "getBlock") {
         //Get the range of a selection and find out the id of the DOM objects.
-        var range = document.getSelection().getRangeAt();
+        var range = document.getSelection().getRangeAt(0);
+        
+        var start = range.startContainer;
+        var text = "lvl: 0 offset: " + range.startOffset.toString() + "\n";
+        var level = 0;
+        text = getPosition(start, text, level);
 
-        sendResponse({ data: "bumm sakalaka" });
+            
+        
+        sendResponse({ data: text});
     }
     else
         sendResponse({}); // snub them.
 });
 
+function getPosition(current, text, level){
+    level++;
+    text +="lvl: " + level + " ";
+    if (current == null)
+        return text;
+    
+    if (current.nodeType == 3) {
+        text += " text "  + "\n";
+        text = getPosition(current.parentElement, text, level);
+    }
+    
+    else if (current.id == ""){
+        var remainingSiblingsCount = 0;
+        var sibling = current;
+        while (current.nextElementSibling !== null){
+            remainingSiblingsCount++;
+            current = current.nextElementSibling;
+        }
+        text +=" rS: " + remainingSiblingsCount  + " class: " + current.className + "\n";
+        text = getPosition(current.parentElement, text, level);
+    }
+    else {
+        text += " id: " + current.id;
+        text += " childs: " + current.childElementCount  +  " class: " + current.className + "\n";
+        text = getPosition(current.parentElement, text, level);
+    }
+    
+    return text;
+        
+        
+}
 
-function getElementsBetweenTree(start, end) {
-    var ancestor = getCommonAncestor(start, end);
+
+
+function getElementsBetweenTree(range) {
+    var ancestor = range.commonAncestorContainer;
+    var start = range.startContainer;
+    var end = range.endContainer;
 
     var before = [];
     while (start.parentNode !== ancestor) {
