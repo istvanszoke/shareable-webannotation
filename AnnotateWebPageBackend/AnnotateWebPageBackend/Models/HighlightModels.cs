d09ef7a1bf8206e89f6c0bd951da8820502be320
lendvaipetra <lendvaipetra092@gmail.com>
2016-11-25 02:10:59 +0100

    Fixing highlightmodel insert.

--- AnnotateWebPageBackend/AnnotateWebPageBackend/Models/HighlightModels.cs
+++ AnnotateWebPageBackend/AnnotateWebPageBackend/Models/HighlightModels.cs
@@ -29,7 +29,7 @@ namespace AnnotateWebPageBackend.Models
                     List<HighlightModel> highlights = new List<HighlightModel>();
                     foreach (var highlight in db.Highlight)
                     {
-                        highlights.Add(new HighlightModel() { id = highlight.id, user_id = highlight.user_id, web_page = highlight.web_page, start = highlight.start, end = highlight.end });
+                        highlights.Add(new HighlightModel() {user_id = highlight.user_id, web_page = highlight.web_page, start = highlight.start, end = highlight.end });
                     }
                     return highlights;
                 }
@@ -49,7 +49,7 @@ namespace AnnotateWebPageBackend.Models
                 {
                     foreach (var highlight in db.Highlight)
                     {
-                        if (highlight.id == id) return new HighlightModel() { id = highlight.id, user_id = highlight.user_id, web_page = highlight.web_page, start = highlight.start, end = highlight.end };
+                        if (highlight.id == id) return new HighlightModel() {user_id = highlight.user_id, web_page = highlight.web_page, start = highlight.start, end = highlight.end };
                     }
 
                     return null;
@@ -71,7 +71,7 @@ namespace AnnotateWebPageBackend.Models
                 foreach (var highlight in db.Highlight)
                 {
                     if (highlight.user_id.Equals(userId) && highlight.web_page.Equals(url))
-                        highlights.Add(new HighlightModel() { id = highlight.id, user_id = highlight.user_id, web_page = highlight.web_page, start = highlight.start, end = highlight.end });
+                        highlights.Add(new HighlightModel() {user_id = highlight.user_id, web_page = highlight.web_page, start = highlight.start, end = highlight.end });
                 }
                 return highlights;
 
@@ -82,47 +82,13 @@ namespace AnnotateWebPageBackend.Models
         {
             try
             {
-                HighlightModel old = null;
                 using (var db = new AnnotateWebPageDBEntities())
                 {
-                    old = GetHighlight(highlight.id);
-                }
-                if (old == null)
-                {
-                    // generate new id
-                    using (var db = new AnnotateWebPageDBEntities())
-                    {
-                        //var nextId = db.Highlight.ToList().Max(hg => hg.id) + 1;
-                        //highlight.id = nextId;
-                        db.Highlight.Add(new Highlight() { id = old.id, user_id = old.user_id, web_page = old.web_page, start = old.start, end = old.end });
-                        db.SaveChanges();
-                    }
-
-                    return old;
-                }
-                else //update
-                {
-                    Highlight updateHighlight =null;
-                    using (var db = new AnnotateWebPageDBEntities())
-                    {
-                        updateHighlight = db.Highlight.Where(s => s.id == highlight.id).FirstOrDefault<Highlight>();
-                    }
-
-                    if (updateHighlight != null)
-                    {
-                        updateHighlight.user_id = highlight.user_id;
-                        updateHighlight.web_page = highlight.web_page;
-                        updateHighlight.start = highlight.start;
-                        updateHighlight.end = highlight.end;
-                    }
-
-                    using (var db = new AnnotateWebPageDBEntities())
-                    {
-                        db.Entry(updateHighlight).State = System.Data.Entity.EntityState.Modified;
-                        db.SaveChanges();
-                    }
+                    Highlight newHighlight = new Highlight() { user_id = highlight.user_id, web_page = highlight.web_page, start = highlight.start, end = highlight.end };
+                    db.Highlight.Add(newHighlight);
+                    db.SaveChanges();
                     return highlight;
-                }
+                }               
 
             }
             catch (Exception e)
